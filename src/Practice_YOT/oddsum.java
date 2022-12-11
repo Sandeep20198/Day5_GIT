07905da3d8e37729d90e3c18bd2977a19b234f37
sandeep20198 <sandeepsingh20198@gmail.com>
2022-12-11 12:33:30 +0530

    odd sum

--- src/Practice_YOT/oddsum.java
+++ src/Practice_YOT/oddsum.java
@@ -6,13 +6,17 @@ public class oddsum {
 
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the number  ");
 
+        int sum=0;
         int  n= sc.nextInt();
+        System.out.println("The odd numbers are ---");
+
         for (int i=0; i<=n; i++){
             if (i%2!=0){
-                System.out.println("odd"+ i);
-            }
+                System.out.println(i);
+                sum = sum + i;}
         }
-
+        System.out.println("the sum of odd numbers is " + sum);
     }
 }
